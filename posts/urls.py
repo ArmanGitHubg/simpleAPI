9966350d5e8e8c40d10c474bd7708e437d11dcb1
armangithubg <armankhaaa@gmail.com>
2023-08-24 14:53:07 +0400

    blog api using django rest framework

---
+++ posts/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+"""
+# This url configure is replaced by Router
+
+from .views import PostList, PostDetail, UserList, UserDetail
+
+
+urlpatterns = [
+    path("users/", UserList.as_view(), name="user_list"),
+    path("users/<int:pk>/", UserDetail.as_view(), name="user_detail"),
+    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
+    path("", PostList.as_view(), name="post_list"),
+    
+]
+
+"""
+
+
+from .views import UserViewSet, PostViewSet
+router = SimpleRouter()
+router.register("users",UserViewSet, basename="users")
+router.register("", PostViewSet, basename="posts ")
+urlpatterns = router.urls
